c5435ce8cb41cf7346b52f1f962b6c9a1ae46075
StasMykhailov <s.m.mykhailov@gmail.com>
2016-05-25 20:46:57 +0300

    First commit hw8

---
+++ hw8f.php
@@ -0,0 +1,38 @@
+// Task 1
+
+<?php
+
+function pifagor($x1, $x2, $x3, $y1, $y2, $y3) {
+    $a = ($x2 - $x1) ** 2 + ($y2 - $y1) ** 2;
+    $b = ($x3 - $x1) ** 2 + ($y3 - $y1) ** 2;
+    $c = ($x3 - $x2) ** 2 + abs($y3 - $y2) ** 2;
+
+    if (($a + $b == $c ) || ($a + $c == $b) || ($b + $c == $a) ) {
+        $res = ' ïğÿìîóãîëüíûé.';
+    } else {
+        $res = ' íå ïğÿìîóãîëüíûé';
+    }
+    return 'Âàø òğåóãîëüíèê'.$res;
+
+}
+?>
+
+// Task 2
+<?php
+
+
+function pifagorCount($maxSideLen) {
+    $count = 0;
+    for ($i = 1; $i < $maxSideLen; $i++){
+        for ($n = $i; $n < $maxSideLen; $n++){
+            for ($m = $n; $m < $maxSideLen; $m++){
+                if ((($i ** 2 + $n ** 2) === ($m ** 2)) || (($i ** 2 + $m ** 2) === ($n ** 2)) ||
+                    (($m ** 2 + $n ** 2) === ($i ** 2))) {
+                    $count++;
+                }
+
+            }
+        }
+    }
+    return $count;
+}
