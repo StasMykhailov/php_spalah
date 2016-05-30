ffd3368e758a20eabef5784962514388874bcd00
StasMykhailov <s.m.mykhailov@gmail.com>
2016-05-30 19:30:16 +0300

    first

---
+++ first.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stas
+ * Date: 13.04.2016
+ * Time: 19:10
+ */
+/*$a = 5;
+while ($a++<10) echo $a.'<br>';
+$a = 5;
+$b = '5';
+if ($a==$b) echo 'True';
+for ($i = 0; $i<5; $i++) {
+    echo $i.'<br>';
+}
+/*$a = 7;
+$b = 8;
+if ($a>$b) {
+    echo $a;
+} elseif ($b>$a) {
+    echo $b;
+}*/
+/*$a =60;
+if ($a>=50) {
+    echo $a*$a;
+} elseif (i>10 and i<30){
+    echo 0;
+} else { echo 'ошибка';
+}*/
+/*$i = 0;
+while (i<=100) {
+    echo $i;
+    $i++;
+}*/
+/*$i = 0;
+do {
+    echo $i;
+    $i++;
+} while (i<=100);*/
+/*for ($i=500; $i>=0; $i-=5 ) {
+    echo $i.'<br>';
+}*/
+/*for ($i=0; $i<=10000; $i++ ) {
+    if ($i%3==0 and $i%5==0) {
+        echo $i.'<br>';
+    }
+}*/
+$i = 0;
+while ($i<=100) {
+    echo $i;
+    $i+=2;
+}
+?>
